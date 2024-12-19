a25c2328508507cfeff7c647716900f51a8650d3
Ritzl <benceritzl@gmail.com>
2024-12-19 12:02:59 +0100

    12-19

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [side, setSide] = useState("");
+  const [height, setHeight] = useState("");
+  const [volume, setVolume] = useState("");
+
+  function startCalc(){
+    const res = 1.0/3.0 * Math.pow(side, 2) * height;
+    setVolume(res);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Gúla térfogatszámítás</Text>
+
+      <Text>Oldal:</Text>
+      <TextInput 
+      style={styles.input}
+      onChangeText={side => setSide(side)}>
+      </TextInput>
+
+      <Text>Magasság:</Text>
+      <TextInput 
+      style={styles.input}
+      onChangeText={height => setHeight(height)}>
+      </TextInput>
+
+      <Button
+      style={styles.input}
+      onPress={startCalc}
+      title='Számítás'
+      ></Button>
+
+      <Text style={styles.volume}>Térfogat:</Text>
+      <TextInput 
+      style={styles.input}
+      value={volume}>
+      </TextInput>
+
+      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +54,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    width: 150,
+    margin: 12,
+    borderWidth: 2,
+    borderColor: 'blue',
+    padding: 10,
+  },
+
+  volume: {
+    marginTop: 10
+  },
+
+  
+
+
+  
 });
